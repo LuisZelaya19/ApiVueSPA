57a772af76a7381212edbec3f07830dfb57714da
LuisZelaya19 <joluzel19@gmail.com>
2021-07-28 22:37:16 -0600

    Add PostController, PostResource, Pagination

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -1,35 +1,51 @@
 <template>
   <div>
-    <table class="table table-bordered">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colspan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Post text</th>
+                <th scope="col">Created at</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="post in posts.data">
+                <td>{{post.title}}</td>
+                <td>{{post.post_text}}</td>
+                <td>{{post.created_at}}</td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+          <pagination
+            :data="posts"
+            @pagination-change-page="getResults"
+          ></pagination>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+export default {
+  data() {
+    return {
+      posts: {},
+    };
+  },
+  mounted() {
+    this.getResults();
+  },
+  methods: {
+    getResults(page = 1) {
+      axios.get("/api/posts?page=" + page).then((response) => {
+        this.posts = response.data;
+      });
+    },
+  },
+};
 </script>
